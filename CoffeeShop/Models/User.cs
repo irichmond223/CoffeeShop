a616e46ccacbdc32573ccc12ecad6f48f9ccf110
ilona <ilonabarashkina@gmail.com>
2020-01-23 18:23:40 -0500

    Added extra inputs

--- CoffeeShop/Models/User.cs
+++ CoffeeShop/Models/User.cs
@@ -9,6 +9,9 @@ namespace CoffeeShop.Models
 
     {
         public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Phone { get; set; }
